28546959ee1d02043c974ab08cb2e1275c3ff8d6
Charlene <leclerc.charlene0602@gmail.com>
2024-02-09 10:43:23 +0100

    Ajout du jeu

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const choices = ['Pierre', 'Feuille', 'Ciseaux'];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Fonction pour générer le choix de l'ordinateur
+function computerPlay() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
-setupCounter(document.querySelector('#counter'))
+// Fonction pour déterminer le résultat du jeu
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Égalité!";
+    } else if (
+        (playerSelection === 'pierre' && computerSelection === 'ciseaux') ||
+        (playerSelection === 'feuille' && computerSelection === 'pierre') ||
+        (playerSelection === 'ciseaux' && computerSelection === 'feuille')
+    ) {
+        return "Vous avez gagné!";
+    } else {
+        return "Vous avez perdu!";
+    }
+}
+
+// Fonction principale du jeu
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choisissez Pierre, Feuille ou Ciseaux :");
+        const computerSelection = computerPlay();
+        console.log(`Vous avez choisi : ${playerSelection}`);
+        console.log(`L'ordinateur a choisi : ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+// Lancer le jeu
+game();
